41f66afa4ea7861f1b92e9de6754bd76e387b00b
Castlefox <michalmskowalczyk@mail.com>
2025-06-08 01:29:46 +0200

    labels display correctly on main grid

--- BackendClassLibrary/Client.cs
+++ BackendClassLibrary/Client.cs
@@ -1,14 +1,21 @@
-﻿#define CLEAR
+﻿using System.ComponentModel;
 namespace bakk_project_task
 {
     public class Client
     {
+        [DisplayName("Id")]
         public required long? Id { get; set; }
+        [DisplayName("Imię")]
         public required string? FirstName { get; set; }
+        [DisplayName("Nazwisko")]
         public required string? LastName { get; set; }
+        [DisplayName("Adres")]
         public required string? Address { get; set; }
+        [DisplayName("Email")]
         public required string? Email { get; set; }
+        [DisplayName("Numer Telefonu")]
         public required string? PhoneNumber { get; set; }
+        [DisplayName("Status")]
         public required string? Status { get; set; }
     }
 }
