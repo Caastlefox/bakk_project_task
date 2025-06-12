61d9ea3fa89a10971126c64aac2b48c09474d65b
Castlefox <michalmskowalczyk@mail.com>
2025-06-12 02:46:25 +0200

    Tag column made readonly for the user

--- BackendClassLibrary/Entry.cs
+++ BackendClassLibrary/Entry.cs
@@ -5,6 +5,7 @@ namespace bakk_project_task
     public class Entry(string name, char tag, long id = -1) : INotifyPropertyChanged
     {
         public string Name { get; set; } = name;
+        [ReadOnly(true)]
         public char Tag { get; set; } = tag;
         [Browsable(false)]
         public long Id { get; set; } = id;
